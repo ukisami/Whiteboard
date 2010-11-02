4e74e8d1eb782a83eb03b1453fe5a5254b367d58
Bowen Sun <bsun@nvidia.com>
2010-11-01 20:05:50 -0700

    Implement Board and Layer Creation

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -1,11 +1,11 @@
 class Layer < ActiveRecord::Base
-	belongs_to :board
-	before_create :randomized_token 
-	
-	
-	def randomized_token 
-		self.token = ActiveSupport::SecureRandom.hex(16)
-	end
-	
-		
-end	
+  belongs_to :board
+  before_create :randomized_token 
+
+
+  def randomized_token
+    self.token = ActiveSupport::SecureRandom.hex(16)
+  end
+
+
+end
