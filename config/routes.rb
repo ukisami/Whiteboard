e95dbfe7c00c5344a0c85f54a28c4f5e09fac67c
Jian P. Chen <jianpchen@jianpchen.(none)>
2010-11-22 23:48:15 -0800

    lol that killed rspec dead

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 
+	map.root :controller => :boards, :action => :index
 end
