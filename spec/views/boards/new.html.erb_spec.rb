a547f27c1ca92ca722eb42509c4def86ef859784
Jian P. Chen <jianpchen@jianpchen.(none)>
2010-11-22 22:28:25 -0800

    remove publication model

--- spec/views/boards/new.html.erb_spec.rb
+++ spec/views/boards/new.html.erb_spec.rb
@@ -15,4 +15,12 @@ describe "/boards/new.html.erb" do
     response.should have_tag("form[action=?][method=post]", boards_path) do
     end
   end
+  
+  it "renders new board form with errors" do
+  	errors_stub = mock(:on => 'lol')
+  	assigns[:board].stub(:errors).and_return(errors_stub)
+  	render
+  	response.should have_tag("span") do
+  	end
+  end
 end
