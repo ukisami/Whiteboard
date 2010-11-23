80dfdffda294184bda35b462207d54a7762ed475
Bowen Sun <bsun@nvidia.com>
2010-11-22 21:53:13 -0800

    Fix chats spec

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,18 +5,18 @@ class ChatsController < ApplicationController
     @board = Board.find params[:board_id]
     @layer = @board.layer_from_token(params[:token])
     @chat = nil
-    if layer
-      @chat = @board.chats.new :author => layer.name, :body => params[:body]
+    if @layer
+      @chat = @board.chats.new :author => @layer.name, :body => params[:body]
     else
-    	@chat = @board.chats.new :author => 'viewer', :body => params[:body]
+      @chat = @board.chats.new :author => 'viewer', :body => params[:body]
     end
 
     respond_to do |format|
-      if @chat and @chat.save
+      if @chat.save
         format.xml  { head :ok }
         format.html { redirect_to board_path(@board, :token => params[:token]) }
       else
-	      redirect_to board_path(@board, :token => params[:token])
+        redirect_to board_path(@board, :token => params[:token])
       end
     end
   end
