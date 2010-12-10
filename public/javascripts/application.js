624f0b7a1ac413256191130daeaf8d5a02284d24
Warren He <microrffr@gmail.com>
2010-12-10 14:16:04 -0800

    client side layer controls

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,29 +4,25 @@ var THUMB_WIDTH = 160;
 var THUMB_HEIGHT = 120;
 var SAVE_INTERVAL = 1000;
 var POLL_INTERVAL = 2000;
-var container, canvas, context, toolbar, publishButton;
+var container, canvas, context, toolbar, publishButton, layerList, editLayers;
 var chat, chatBody;
 var x, y;
 var activeWidth = null;
 var activeColor = null;
 var saveTimer = false;
+var dragging = null;
 
 function init() {
 	findElements();
 	registerChat();
-	if (token) {
+	if (layerid && token) {
 		injectCanvas();
 		registerTools();
 		clickTool(document.getElementById('defaultwidth'));
 		clickTool(document.getElementById('defaultcolor'));
 	}
+	if (layerList.className == 'owned') registerControls();
 	schedulePoll();
-}
-
-function clickTool(t) {
-	var e = document.createEvent('MouseEvents');
-	e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-	t.dispatchEvent(e);
 }
 
 function findElements() {
@@ -34,6 +30,13 @@ function findElements() {
 	chat = document.getElementById('chat');
 	chatBody = document.getElementById('chatbody');
 	toolbar = document.getElementById('toolbar');
+	publishButton = document.getElementById('publish');
+	rearrangeLayers = document.getElementById('rearrangeLayers');
+	layerList = document.getElementById('layerList');
+}
+
+function registerChat() {
+	document.getElementById('chatform').addEventListener('submit', sendChat, false);
 }
 
 function injectCanvas() {
@@ -45,6 +48,10 @@ function injectCanvas() {
 	context.drawImage(img, 0, 0);
 	context.lineCap = 'round';
 	context.lineJoin = 'round';
+	canvas.id = img.id;
+	canvas.style.zIndex = img.style.zIndex;
+	canvas.style.opacity = img.style.opacity;
+	canvas.style.visibility = img.style.visibility;
 	container.insertBefore(canvas, img);
 	container.removeChild(img);
 }
@@ -61,13 +68,26 @@ function registerTools() {
 	document.getElementById('eraser').addEventListener('click', toolEraser, false);
 	container.addEventListener('mousedown', mouseDown, false);
 	document.body.addEventListener('mouseup', mouseUp, false);
-
-	publishButton = document.getElementById('publish');
-	if (publishButton) publishButton.addEventListener('click', publish, false);
 }
 
-function registerChat() {
-	document.getElementById('chatform').addEventListener('submit', sendChat, false);
+function registerControls() {
+	publishButton.addEventListener('click', publish, false);
+	rearrangeLayers.addEventListener('click', unlockOrder, false);
+
+	var visibilities = layerList.getElementsByClassName('visible');
+	for (var i = 0; i < visibilities.length; i++) {
+		visibilities[i].addEventListener('change', saveVisible, false);
+	}
+	var opacities = layerList.getElementsByClassName('opacity');
+	for (var i = 0; i < opacities.length; i++) {
+		opacities[i].addEventListener('change', saveOpacity, false);
+	}
+}
+
+function clickTool(t) {
+	var e = document.createEvent('MouseEvents');
+	e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+	t.dispatchEvent(e);
 }
 
 function toolWidth(e) {
@@ -165,8 +185,18 @@ function handlePoll(json) {
 	if (response.revision <= revision) return;
 	revision = response.revision;
 	for (var layer in response.layers) {
-		if (layer == layerid) continue;
-		document.getElementById('layer' + layer).src = response.layers[layer];
+		var img = document.getElementById('layer' + layer);
+		if (!img) {
+			img = document.createElement('img');
+			img.id = 'layer' + layer;
+			container.appendChild(img);
+		}
+		var updates = response.layers[layer];
+		if (updates.data && layer != layerid) img.src = updates.data;
+		if (updates.order) img.style.zIndex = updates.order;
+		if (updates.opacity) img.style.opacity = updates.opacity / 100;
+		if (updates.visible) img.style.visibility = updates.visible ? 'visible' : 'hidden';
+		// is it worth it to update the layers list?
 	}
 	for (var i in response.chats) {
 		var line = response.chats[i];
@@ -234,4 +264,145 @@ function compose(width, height) {
 		compositeContext.drawImage(layers[i], 0, 0, width, height);
 	}
 	return composite.toDataURL();
+}
+
+function unlockOrder() {
+	rearrangeLayers.innerHTML = 'save';
+	rearrangeLayers.removeEventListener('click', unlockOrder, false);
+	rearrangeLayers.addEventListener('click', saveOrder, false);
+
+	var layers = layerList.getElementsByTagName('li');
+	for (var i = 0; i < layers.length; i++) {
+		layers[i].addEventListener('mousedown', startDrag, false);
+	}
+}
+
+function startDrag(e) {
+	e.preventDefault();
+	dragging = e.currentTarget;
+	document.body.addEventListener('mouseup', stopDrag, false);
+	addSwap();
+}
+
+function addSwap() {
+	var prev, next;
+	if (prev = dragging.previousElementSibling) prev.addEventListener('mouseover', swapUp, false);
+	if (next = dragging.nextElementSibling) next.addEventListener('mouseover', swapDown, false);
+}
+
+function removeSwap() {
+	var prev, next;
+	if (prev = dragging.previousElementSibling) prev.removeEventListener('mouseover', swapUp, false);
+	if (next = dragging.nextElementSibling) next.removeEventListener('mouseover', swapDown, false);
+}
+
+function swapUp(e) {
+	e.preventDefault();
+	removeSwap();
+	var li = e.currentTarget;
+	layerList.removeChild(dragging);
+	layerList.insertBefore(dragging, li);
+	addSwap();
+	resetOrder();
+}
+
+function swapDown(e) {
+	e.preventDefault();
+	removeSwap();
+	var li = e.currentTarget;
+	layerList.removeChild(li);
+	layerList.insertBefore(li, dragging);
+	addSwap();
+	resetOrder();
+}
+
+function stopDrag(e) {
+	e.preventDefault();
+	document.body.removeEventListener('mouseup', stopDrag, false);
+	removeSwap();
+	dragging = null;
+}
+
+function getOrder() {
+	var layers = layerList.getElementsByTagName('li');
+	var order = {};
+	for (var i = 0; i < layers.length; i++) {
+		order[layers[i].id.substr(2)] = layers.length - i - 1;
+	}
+	return order;
+}
+
+function resetOrder() {
+	var order = getOrder();
+	for (var layer in order) {
+		document.getElementById('layer' + layer).style.zIndex = order[layer];
+	}
+}
+
+function saveOrder() {
+	rearrangeLayers.innerHTML = 'rearrange';
+
+	var body = 'token=' + token;
+	var order = getOrder();
+	for (var layer in order) {
+		body += '&' + layer + '=' + order[layer];
+	}
+
+	var layers = layerList.getElementsByTagName('li');
+	for (var i = 0; i < layers.length; i++) {
+		layers[i].removeEventListener('mousedown', startDrag, false);
+	}
+	document.body.removeEventListener('mouseup', stopDrag, false);
+
+	rearrangeLayers.removeEventListener('click', saveOrder, false);
+	rearrangeLayers.innerHTML = 'saving';
+	var xhr = new XMLHttpRequest();
+	xhr.open('PUT', '/boards/' + boardid + '/order');
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState < 4) return;
+		rearrangeLayers.innerHTML = 'saved';
+		setTimeout(function() {
+			rearrangeLayers.innerHTML = 'rearrange';
+			rearrangeLayers.addEventListener('click', unlockOrder, false);
+		}, 1000);
+	};
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.setRequestHeader('Content-Length', body.length);
+	xhr.send(body);
+}
+
+function saveVisible(e) {
+	var input = e.currentTarget;
+	var layer = input.id.substr(7);
+
+	document.getElementById('layer' + layer).style.visibility = input.checked ? 'visible' : 'hidden';
+
+	var body =
+		'token=' + token +
+		'&visible=' + (input.checked ? 'true' : 'false');
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('PUT', '/boards/' + boardid + '/layers/' + layer);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.setRequestHeader('Content-Length', body.length);
+	xhr.send(body);
+}
+
+function saveOpacity(e) {
+	var input = e.currentTarget;
+	var layer = input.id.substr(7);
+
+	var opacity = parseInt(input.value, 10);
+	input.value = opacity;
+	document.getElementById('layer' + layer).style.opacity = opacity / 100;
+
+	var body =
+		'token=' + token +
+		'&opacity=' + encodeURIComponent(opacity);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('PUT', '/boards/' + boardid + '/layers/' + layer);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.setRequestHeader('Content-Length', body.length);
+	xhr.send(body);
 }
